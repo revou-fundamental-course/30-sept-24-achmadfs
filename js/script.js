665010c3e93d63d3f8fdb4b33819ab456e7c6c2a
Achmad Fajar Subekti <fajar.soebekthy@gmail.com>
2024-10-09 20:00:10 +0700

    ke empat belas

--- js/script.js
+++ js/script.js
@@ -3,5 +3,16 @@
 console.log('java script tersambung')
 
 function validateform() {
+    console.log('validasi sudah terpangil')
+    const inputSuhu = document.getElementById('suhu-celsius').Value;
 
+    if (inputSuhu == '') {
+        alert('Input Kosong atau Salah');
+    } else {
+        calculate(inputSuhu);
+    }
+}
+
+function calculate(Value) {
+    console.log(parseInt(Value) + 1);
 }
